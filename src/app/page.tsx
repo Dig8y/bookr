6f60caa5df058df95782905b01016c018f2903fe
Dig8y <137469494+Dig8y@users.noreply.github.com>
2024-09-20 17:59:20 +0100

    initial

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <h1>WELCOME</h1>
+    </main>
+  );
+}
